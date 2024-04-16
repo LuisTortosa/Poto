8827581629e3d4ff624fbd6706b0af466bbccf6a
Luis <Luis@192.168.1.42>
2024-04-16 11:15:31 +0200

    Primera cosa pocha

---
+++ Examenprueba/src/main/java/com/mycompany/examenprueba/clase.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.examenprueba;
+
+/**
+ *
+ * @author Luis
+ */
+public class clase {
+    
+    public double dividir(int num1, int num2){
+        
+        
+        return num1 / num2;
+    }
+    
+}
